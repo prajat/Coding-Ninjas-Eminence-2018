9ed3dc90aaa65d764e9e6d02ea5737d6d2b637de
prajat <rajat.singh1084@gmail.com>
2018-10-04 23:23:24 +0530

    correction in code

--- ASSIGNMENTS_STLUSE/extractuniquecharacters.java
+++ ASSIGNMENTS_STLUSE/extractuniquecharacters.java
@@ -6,39 +6,41 @@ public class extractuniquecharacters {
 
     public static void main(String[] args) {
 
-        String str = "ababacd";
+        String str = "raaaaaaaaajjjjjjjjjaaaaaaajjjjjjjtttttt";
         System.out.println(uniqueChar(str));
     }
     
     public static String uniqueChar(String str) {
-        // Write your code here
-        // HashMap<Character, Integer> map = new HashMap<>();
-        // for (int i = 0; i < str.length(); i++) {
-        //     char cc = str.charAt(i);
-        //     if (map.containsKey(cc)) {
-        //         map.put(cc, map.get(cc) + 1);
+        
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char cc = str.charAt(i);
+            if (map.containsKey(cc)) {
+                map.put(cc, map.get(cc) + 1);
 
-        //     } else {
-        //         map.put(cc, 1);
-        //     }
-        // }
-        // StringBuilder sb = new StringBuilder();
-        // for (Map.Entry<Character, Integer> i : map.entrySet()) {
-        //     sb.append(i.getKey().charValue());
-        // }
-        // return sb.toString();
-
-        HashSet<Character> set = new HashSet<>();
-        for(int i=0;i<str.length();i++){
-            set.add(str.charAt(i));
-
+            } else {
+                map.put(cc, 1);
+            }
         }
-        Iterator iterator=set.iterator();
         StringBuilder sb = new StringBuilder();
-        while(iterator.hasNext()){
-            sb.append(iterator.next());
-
+        for (Map.Entry<Character, Integer> i : map.entrySet()) {
+            sb.append(i.getKey().charValue());
+            
+            
         }
         return sb.toString();
+
+        // HashSet<Character> set = new HashSet<>();
+        // for(int i=0;i<str.length();i++){
+        //     set.add(str.charAt(i));
+
+        // }
+        // Iterator iterator=set.iterator();
+        // StringBuilder sb = new StringBuilder();
+        // while(iterator.hasNext()){
+        //     sb.append(iterator.next());
+
+        // }
+        // return sb.toString();
     }
 }
