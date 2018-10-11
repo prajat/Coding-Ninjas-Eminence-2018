ca7a5690ae695ae2faedfa1260320e22b48f0256
prajat <rajat.singh1084@gmail.com>
2018-10-12 00:26:24 +0530

    ques solved

--- Assignment-Backtracking,bs,mergesort/distributecandies.java
+++ Assignment-Backtracking,bs,mergesort/distributecandies.java
@@ -10,25 +10,25 @@ public class distributecandies {
         Scanner s = new Scanner(System.in);
         int t = s.nextInt();
         for (int j= 1; j <= t; j++) {
-            int n = s.nextInt();
-            int k = s.nextInt();
-            int[] arr = new int[n];
+            long n = s.nextLong();
+            long k = s.nextLong();
+            long[] arr = new long[n];
             for (int i = 0; i < n; i++) {
-                arr[i] = s.nextInt();
+                arr[i] = s.nextLong();
             }
             System.out.println(solve(arr, k));
         }
     }
 
-    public static int solve(int[] arr, int k) {
-        int start = 0;
-        int[] temp = arr;
+    public static int solve(long[] arr, long k) {
+        long start = 0;
+        long[] temp = arr;
         Arrays.sort(temp);
-        int end = temp[temp.length - 1];
+        long end = temp[temp.length - 1];
 
         int ans = 0;
         while (start < end) {
-            int mid = (start + end) / 2;
+            long mid = (start + end) / 2;
             if (check(arr, mid, k)) {
                 ans = mid;
                 start = mid + 1;
@@ -41,13 +41,13 @@ public class distributecandies {
 
     }
 
-    public static boolean check(int[] arr, int mid, int k) {
+    public static boolean check(long[] arr, long mid, long k) {
         int count = 0;
         for (int i = 0; i < arr.length; i++) {
             int smallans = arr[i] / mid;
             count += smallans;
 
-            if (count == k) {
+            if (count >= k) {
                 return true;
             }
         }
---
+++ Assignment-Backtracking,bs,mergesort/sudukosolver.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/**
+ * sudukosolver
+ */
+public class sudukosolver {
+
+    public static void main(String[] args) {
+
+    }
+    
+    public static boolean sudokuSolver(int board[][]) {
+
+        int row=-1;
+        int col = -1;
+
+    }
+    
+}
