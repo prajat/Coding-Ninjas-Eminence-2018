1e4f3aee9767f7a0f812822babdb39f818b9042c
prajat <rajat.singh1084@gmail.com>
2018-10-20 22:49:15 +0530

    correction

--- MixedProblem/wordbreak1.java
+++ MixedProblem/wordbreak1.java
@@ -6,27 +6,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
-        HashMap<String, Integer> map = new HashMap<>();
+       // HashMap<String, Integer> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
         for (int i = 1; i <= n; i++) {
             String s1 = s.nextLine();
-            map.put(s1, 1);
+            //map.put(s1, 1);
+            set.add(s1);
         }
         String word = s.nextLine();
 
-        wordbreak(word, map, "");
+        wordbreak(word, set, "");
     }
 
-    public static void wordbreak(String word, HashMap<String, Integer> map, String result) {
+    public static void wordbreak(String word, HashSet<String> set, String result) {
 
         for (int i = 1; i <= word.length(); i++) {
             String prefix = word.substring(0, i);
-            if (map.containsKey(prefix)) {
+            if (set.contains(prefix)) {
                 if (i == word.length()) {
                     result += prefix;
                     System.out.println(result);
                     return;
                 }
-                wordbreak(word.substring(i, word.length() - i), map, result + prefix + " ");
+                wordbreak(word.substring(i, word.length() - i), set, result + prefix + " ");
             }
         }
 
