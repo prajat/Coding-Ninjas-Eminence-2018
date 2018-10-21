a6cf76417ac6c457b63ab0792768f274e8e98354
prajat <rajat.singh1084@gmail.com>
2018-10-21 21:36:22 +0530

    ques solved

--- MixedProblem/professorsassignment.java
+++ MixedProblem/professorsassignment.java
@@ -1,27 +1,32 @@
-import java.util.*;
-/**
- * professorsassignment
- */
-public class professorsassignment {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class Main {
+
+	
+	public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        //int n = s.nextInt();
-        int mod = 1000000007;
-        int n = 5;
+        long n = s.nextLong();
+        int mod=1000000007;
 
         //int inv = 0.5 % mod;
-        int sum = 0;
-        for (int i = 1; i <= n;) {
-            int x = n / i;
-            int j = n / x;
-            int y = (j - i + 1)  * ((x * (x + 1))/2) ;
-
-            //sum = (sum % mod + y % mod) % mod;
-            sum += y;
-            i = j + 1;
-
+       long i=1;
+        long sum=0;
+      	long t=500000004;
+ 
+      	while(i<=n){
+      	    long temp;
+          	long x=n/i;
+          	long j=n/x;
+          	long y=(((((x)%mod)*((x+1)%mod))%mod)*t)%mod;
+ 
+      	   temp=(((((j-i+1)%mod)*((i+j)%mod))%mod)*t)%mod;
+ 
+          	sum=((sum+(temp*y)%mod))%mod;
+ 
+          	i=j+1;
         }
-        System.out.println(sum);
+      	
+      System.out.println(sum);
     }
+
 }
