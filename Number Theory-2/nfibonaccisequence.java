a532ef367dee89f92d69969b7fe4f6fe669ea322
prajat <rajat.singh1084@gmail.com>
2018-11-05 00:42:22 +0530

    correction in codes

--- Number Theory-2/nfibonaccisequence.java
+++ Number Theory-2/nfibonaccisequence.java
@@ -1,58 +1,62 @@
-public class Solution {
+void multiply(signed long long A[2][2], signed long long M[2][2])
+{
+	A[0][0] %= 1000000007;
+	A[0][1] %= 1000000007;
+	A[1][0] %= 1000000007;
+	A[1][1] %= 1000000007;
 
-    public static long fiboSum(long m, long n) {
-        // Write your code here
-        long first = fib(m + 2);
-        long second = fib(n + 1);
-        return (first - second);
-    }
+	M[0][0] %= 1000000007;
+	M[0][1] %= 1000000007;
+	M[1][0] %= 1000000007;
+	M[1][1] %= 1000000007;
 
-    public static void multiply(long[][] A, long[][] M) {
-        A[0][0] %= 1000000007;
-        A[0][1] %= 1000000007;
-        A[1][0] %= 1000000007;
-        A[1][1] %= 1000000007;
+	signed long long first = (( A[0][0] * M[0][0] ) %1000000007  + ( A[0][1] * M[1][0] ) %1000000007)%1000000007; 	
+	signed long long second = (( A[0][0] * M[0][1] ) %1000000007 + ( A[0][1] * M[1][1] ) %1000000007)%1000000007; 
+	signed long long third = (( A[1][0] * M[0][0] ) %1000000007 +  ( A[1][1] * M[1][0] ) %1000000007)%1000000007; 
+	signed long long fourth = (( A[1][0] * M[0][1] ) %1000000007 + ( A[1][1] * M[1][1] ) %1000000007)%1000000007; 
 
-        M[0][0] %= 1000000007;
-        M[0][1] %= 1000000007;
-        M[1][0] %= 1000000007;
-        M[1][1] %= 1000000007;
+	A[0][0] = first%1000000007;
+	A[0][1] = second%1000000007;
+	A[1][0] = third%1000000007;
+	A[1][1] = fourth%1000000007;
+}
 
-        long first = (A[0][0] * M[0][0]) % 1000000007 + (A[0][1] * M[1][0]) % 1000000007;
-        long second = (A[0][0] * M[0][1]) % 1000000007 + (A[0][1] * M[1][1]) % 1000000007;
-        long third = (A[1][0] * M[0][0]) % 1000000007 + (A[1][1] * M[1][0]) % 1000000007;
-        long fourth = (A[1][0] * M[0][1]) % 1000000007 + (A[1][1] * M[1][1]) % 1000000007;
+void power(signed long long A[2][2] , signed long long n)
+{
+	if(n <= 1)
+		return;
 
-        A[0][0] = first % 1000000007;
-        A[0][1] = second % 1000000007;
-        A[1][0] = third % 1000000007;
-        A[1][1] = fourth % 1000000007;
-    }
+	power(A,n/2);
 
-    public static void power(long[][] A, long n) {
-        if (n <= 1)
-            return;
+	multiply(A,A);
 
-        power(A, n / 2);
+	if(n%2 == 1) // if n is odd
+	{
+		signed long long M[2][2] =  { {1,1},{1,0} };
+		multiply(A,M);
+	}
 
-        multiply(A, A);
 
-        if (n % 2 == 1) // if n is odd
-        {
-            long[][] M = { { 1, 1 }, { 1, 0 } };
-            multiply(A, M);
-        }
 
-    }
+}
 
-    public static long fib(long n) {
-        long[][] A = { { 1, 1 }, { 1, 0 } };
+signed long long fib(signed long long n)
+{
+	signed long long A[2][2] =  { {1,1},{1,0} };
 
-        if (n == 0)
-            return 0;
+	if(n == 0 )
+		return 0;
 
-        power(A, n - 1);
-        return A[0][0] % 1000000007;
-    }
+	power(A,n-1);
+	return (A[0][0]+1000000007)%1000000007;
+}
+
+
+signed long long fiboSum(signed long long m,signed long long n)
+{
+    
+	signed long long first = fib(n+2);
+	signed long long second = fib(m+1);
+	return ( (first - second)+1000000007 )%1000000007;
 
 }
