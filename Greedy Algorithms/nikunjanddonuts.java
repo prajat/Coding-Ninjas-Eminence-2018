fd2fe0209367f6f89be63d8f88fe6fdf6e3245e2
prajat <rajat.singh1084@gmail.com>
2019-02-04 23:02:42 +0530

    ques solved

---
+++ Greedy Algorithms/nikunjanddonuts.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+import java.util.Scanner;
+
+public class nikunjanddonuts{
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = s.nextInt();
+
+        }
+
+        Arrays.sort(arr);
+        long miles = 0;
+        long count = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            miles += count * arr[i];
+            count *= 2;
+        }
+
+        System.out.print(miles);
+
+    }
+
+}
