8af6d1a944286898509c5eab40286b24ab104fc8
prajat <rajat.singh1084@gmail.com>
2019-02-05 21:59:33 +0530

    changes in directory

--- LEVEL-1 TA TEST/teamselection.java
+++ LEVEL-1 TA TEST/teamselection.java
@@ -6,9 +6,7 @@ import java.util.*;
 public class subsetsoffixedsize {
 
     public static void main(String[] args) {
-
-       
-
+        
         Scanner s = new Scanner(System.in);
         int N = s.nextInt();
         int[] arr = new int[N];
